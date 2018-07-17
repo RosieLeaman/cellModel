bc4fd8c2a70471b432ecd6ee7065719ca772e7a2
RosieLeaman <rosie.leaman@gmail.com>
2018-07-17 14:39:07 +0100

    skeleton first commit

---
+++ mainModel.m
@@ -0,0 +1,36 @@
+function mainModel
+
+% set up the model
+
+% while time is less than max time
+
+    % see if we need to insert a new BAM
+    
+        % if yes add a BAM to a new randomly chosen location
+        
+        % add new material (protein)
+       
+    % see if we need to add new lptD (or how many?)
+    
+        % if yes pick a BAM
+        
+        % add new material (lipid)
+        
+        
+    % move polygon vertices
+    
+    
+    % move insertion points
+    
+    
+    
+    % resolve boundary issues
+    
+    
+    % calculate anything you want to calculate
+    
+    
+    % increase time
+    
+    
+    
