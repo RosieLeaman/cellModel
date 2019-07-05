cbd8c4c66767f57eff399a556d01ba56256e5193
RosieLeaman <rosie.leaman@gmail.com>
2019-07-05 15:30:30 +0100

    Added comments and cleaned up calcIntegrandVectorised.m removed all the unnecessary testing arguments

--- calcIntegrandVectorised.m
+++ calcIntegrandVectorised.m
@@ -1,9 +1,30 @@
-function integrands = calcIntegrandVectorised(t,r0,r0rotation,splineX,splineY,plotYes,normalYes,a,b)
+% this function calculates the surface tension integrand at the given point
+% r0. It expects a vector of parameter values, t
+% the parametrisation is EXPECTED to be BY ARC LENGTH
+
+% INPUTS:
+% t; 1xn vector of parameter values at which to calculate the integrand. I
+%    believe that it is expected to be a row vector
+% r0; 1x2 vector. the specific point [r0x,r0y] (point, not parameter value!)
+%    at which the integral is to be calculated. 
+% r0rotation; 2x2 rotation matrix which rotates the plane so that the point
+%    r0 has unit normal (1,0) and unit tangent (0,1)
+% splineX; a spline object which interpolates the x co-ordinates given
+%    parameter values
+% splineY; a spline object which interpolates the y co-ordinates given
+%    parameter values
+% plotYes; 0 or 1 - 0 no plotting, 1 plotting
+
+%OUTPUTS
+
+% integrands; a 1xt row vector containing the integrand at each input point
+%             in t
+
+function integrands = calcIntegrandVectorised(t,r0,r0rotation,splineX,splineY,plotYes)
 
 % we want to return the integrand at every point p(t)
 
-% first we find the points
-
+% first we find the points which we do by using the splines to interpolate
 points = zeros(numel(t),2);
 
 points(:,1) = ppval(splineX,t);
@@ -17,8 +38,8 @@ points = (r0rotation*points')';
 % we must also rotate r0
 r0 = (r0rotation*r0')';
 
-% we also need the tangents at these points
-
+% we also need the tangents at these points which we get from the original
+% splines.
 [tangents,~] = findTangentFromSplines(t,splineX,splineY,0);
 
 unitTangents = tangents;
@@ -29,10 +50,12 @@ end
 % these also have to be rotated the same as the points
 % it may be a better idea to re-find the tangents from the new points, not
 % sure atm
-rotatedTangents = (r0rotation*tangents')';
+% the non-unit tangents are needed if having to calculate ds
+%rotatedTangents = (r0rotation*tangents')';
 rotatedUnitTangents = (r0rotation*unitTangents')';
 
 if plotYes == 1
+    % plot the tangents to check they look legit
     figure; hold on;
     plot(points(:,1),points(:,2),'x-')
     for i = 1:25:size(points,1)
@@ -42,68 +65,31 @@ end
 
 % then we can work out the integrand easy
 integrands = zeros(1,numel(t));
-integrandsX = zeros(1,numel(t));
-integrandsY = zeros(1,numel(t));
-Jexpressions = zeros(1,numel(t));
-ds = zeros(1,numel(t));
 
 for i=1:numel(integrands)
-    [integrandX,Jexpression] = calcIntegrand(r0,points(i,:),rotatedUnitTangents(i,:));
+    % note it is OK to have r0 and points here as we rotated them earlier
+    integrandX = calcIntegrand(r0,points(i,:),rotatedUnitTangents(i,:));
+
+    % sometimes might need to multiply by ds
+    %absDs = sqrt(rotatedTangents(i,1)^2+rotatedTangents(i,2)^2);
     
-   % this needs reordering, what on earth is this nonsense atm
+    % however if we parametrise by arc length s, then ds = 1, so we don't
+    % need to
     
-    % save these, remembering to multiply the integrand by |ds|
-    absDs = sqrt(rotatedTangents(i,1)^2+rotatedTangents(i,2)^2);
-    ds(i) = absDs;
-    
-    absDs = 1;
-    
-    integrandsX(i) = integrandX*absDs/(-2*pi);
+    integrands(i) = integrandX/(-2*pi);
+    % we have to divide by -2pi here as the J expression calculated below
+    % is actually -2pi*J
    
-    Jexpressions(i) = Jexpression;
-    
-    integrands(i) = integrandsX(i);
 end
-
-% when parametrised by theta, for an ellipse with x stretch a and y stretch
-% b, the correct comparison functions are given by:
-
-% note that these are parametrised so that the integral range is 0-1 NOT
-% 0-2pi
-
-% ENTIRE INTEGRAL:
-% fun5 = @(z) (2*pi*a*(a^2*sin(2*pi*z).^2 + b^2*cos(2*pi*z).^2).^(1/2).*(cos(2*pi*z) - 1).*((a^2.*sin(2*pi*z).^2.*(b^2*sin(2*pi*z).^2 - a^2.*(cos(2*pi*z) - 1).^2))./(a^2.*sin(2*pi*z).^2 + b^2.*cos(2*pi*z).^2) - (b^2*cos(2*pi*z).^2.*(b^2.*sin(2*pi*z).^2 - a^2.*(cos(2*pi*z) - 1).^2))./(a^2.*sin(2*pi*z).^2 + b^2.*cos(2*pi*z).^2) + (4*a^2*b^2.*cos(2*pi*z).*sin(2*pi*z).^2.*(cos(2*pi*z) - 1))./(a^2.*sin(2*pi*z).^2 + b^2.*cos(2*pi*z).^2)))./(b^2.*sin(2*pi*z).^2 + a^2.*(cos(2*pi*z) - 1).^2).^2;
-
-% x and y coordinates
-% xvalues = @(z) a*cos(2*pi*z);
-% yvalues = @(z) b*sin(2*pi*z);
-
-% tangent and unit tangents
-% tangentXunit = @(z) -a*sin(2*pi*z)./sqrt((a*sin(2*pi*z)).^2+(b*cos(2*pi*z)).^2);
-% tangentYunit = @(z) b*cos(2*pi*z)./sqrt((a*sin(2*pi*z)).^2+(b*cos(2*pi*z)).^2);
-% 
-% tangentX = @(z) -2*pi*a*sin(2*pi*z);
-% tangentY = @(z) 2*pi*b*cos(2*pi*z);
-
-% ds = sqrt(dx/dt^2+dy/dt^2)
-% funDs = @(z) 2*pi*(a^2*sin(2*pi*z).^2 + b^2*cos(2*pi*z).^2).^(1/2);
 
 if plotYes == 1
     figure;
     plot(integrands)
 end
 
-if normalYes == 1
-    disp('x integrand only')
-    integrands = integrandsX;
-elseif normalYes == 2
-    disp('y integrand only')
-    integrands = integrandsY;
 end
 
-end
-
-function [integrandX,Jexpression] = calcIntegrand(r0,rs,t)
+function integrandX = calcIntegrand(r0,rs,t)
 % in the case where we rotate, we only need to calculate integrandX 
 
 dr = rs - r0;
@@ -121,28 +107,5 @@ end
 function result = calcJexpression(dX,dY,tX,tY)
 
 result = tX*tX*(dY*dY-dX*dX) - 4*tX*tY*dX*dY + tY*tY*(dX*dX-dY*dY);
-
-end
-
-function [integrandX,integrandY,Jexpression,checkX,checkY] = calcIntegrand2(r0,rs,t)
-
-dr = rs - r0;
-
-square = (dr(1).^2 + dr(2).^2);
-
-Jexpression = calcJexpression2(dr(1),dr(2),t(1),t(2),square);
-
-integrandX = (dr(1)/(square))*Jexpression;
-
-integrandY = (dr(2)/(square))*Jexpression;
-
-checkX = dr(1)/(square.^2);
-checkY = dr(2)/(square.^2);
-
-end
-
-function result = calcJexpression2(dX,dY,tX,tY,square)
-
-result = tX*tX*(1-2*(dX*dX/square)) - 4*tX*tY*(dX*dY/square) + tY*tY*(1-2*(dY*dY/square));
 
 end
